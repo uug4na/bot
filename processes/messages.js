cfdeb27ec50a36b40b689c3ea0b7b93e4832da9b
yo <uuganbayar@infinite.mn>
2023-02-16 00:22:50 -0800

    final 2

--- processes/messages.js
+++ processes/messages.js
@@ -1,287 +1,300 @@
-const request = require('request')
-const axios = require('axios')
-const fb = require('fb')
-var jp = require('jsonpath')
+const request = require("request");
+const axios = require("axios");
+const fb = require("fb");
+var jp = require("jsonpath");
+const { link } = require("fs");
 
-require('dotenv').config()
+require("dotenv").config();
 
 fb.setAccessToken(process.env.PAGE_ACCESS_TOKEN);
 
 module.exports = async function processMessage(event) {
+  if (!event.message.is_echo) {
+    const url =
+      "https://graph.facebook.com/v15.0/me/messages?access_token=" +
+      process.env.PAGE_ACCESS_TOKEN;
+    const nameUrl =
+      `https://graph.facebook.com/${event.sender.id}?fields=name,profile_pic&access_token=` +
+      process.env.PAGE_ACCESS_TOKEN;
+    console.log("MESSAGE DATAS > ", event.message);
 
-    if (!event.message.is_echo) {
+    const message = event.message.text;
+    const senderID = event.sender.id;
+    const number = event.message.text;
 
+    var debt = "";
+    var expireDate = "";
+    var dbUserName = "";
+    var clientName = "";
+    var clientProUrl = "";
+    var message_id = "";
+    var tmp = "";
 
-
-        const url = 'https://graph.facebook.com/v15.0/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN
-        const nameUrl = `https://graph.facebook.com/${event.sender.id}?fields=name,profile_pic&access_token=` + process.env.PAGE_ACCESS_TOKEN
-        const chatUrl = ``
-        console.log("MESSAGE DATAS > ", event.message)
-
-        const message = event.message.text
-        const senderID = event.sender.id
-        const number = event.message.text
-
-        console.log(`https://www.messenger.com/t/132275839651065/${senderID}/?mid=${messageID}`)
-        
-
-
-        var debt = ""
-        var expireDate = ""
-        var dbUserName = ""
-        var clientName = ""
-        var clientProUrl = ""
-        var message_id = "" 
-        
-        
-        try{
-            const response = await axios.get(nameUrl)
-            console.log("Data > ",response.data)
-            clientName = response.data.name
-            clientProUrl = response.data.profile_pic
-            console.log(`CLIENTNAME > ${clientName}`)
-            console.log(`PROFILE LINK > ${clientProUrl}`)
-        }catch(err){
-            console.log(err)
-        }
-
-
-        const options = {
-            url: `https://graph.facebook.com/v15.0/${senderID}/conversations`,
-            qs: {
-              access_token: process.env.PAGE_ACCESS_TOKEN
-            }
-          };
-          
-          request(options, function (error, body) {
-            if (error) throw new Error(error);
-          
-            console.log(body);
-            message_id = jp.query(body, '$.data[:1].link')
-          });
-
-        var names = []
-        var resLocations = []
-        var workHours = []
-        try{
-            const resp = await axios.post('http://13.52.218.164:8000/api/v1/terminals/getLocations')
-            const locations = resp.data.data
-            for(const shits in locations){
-                const lmao = JSON.parse(JSON.stringify(locations[shits]))
-                names.push(lmao.name)
-                resLocations.push(lmao.location)
-                workHours.push(lmao.working_hours)
-            }
-        }catch(err){
-            console.log(err);
-        }
-
-        var text = ""
-        for(let i=0; i<names.length; i++){
-            text += "\n" + names[i] + ", Locations: " + resLocations[i] + ", Work Hours: " + workHours[i] + "\n" 
-        }
-
-        const backurl = 'http://13.52.218.164:8000/api/v1/loans/getByPhone'
-        const backOptions = {
-            url: backurl,
-            method: 'POST',
-            json: {
-                "phone": number,
-                "psid": senderID,
-                "name": clientName,
-                "profile_pic": clientProUrl,
-                "message_id": message_id
-            }
-        } 
-        request.post(backOptions, (err, body) => {
-            if(!err){
-                console.log(JSON.stringify(body))   
-                tmp = JSON.stringify(body)
-                const obj = JSON.parse(tmp)
-                try{
-                    console.log("FSAFDA> ", obj.body.data.repayment.current_pay_amount)
-                    debt = obj.body.data.repayment.current_pay_amount
-                    dbUserName = obj.body.data.repayment.customer_name
-                    expireDate = obj.body.data.repayment.current_pay_date
-                }catch(err){
-                    debt = null
-                }
-            }else{
-                console.log(err)
-            }
-
-
-            if(!debt && message != '2' && message != '1' && message != '3' && message != '4' && message.length < 16){
-                const errorOptions = {
-                    url: url,
-                    method: 'POST',
-                    json: true,
-                    body: {
-                        "recipient": {
-                            "id": senderID
-                        },
-                        "message": {
-                            "text": `${message} Is Not Registered\nBut You Can Register, Our Loan Amounts: \n440PHP\n660PHP\n880PHP\n1100PHP\n1300PHP\n1700PHP`
-                        }
-                    }
-                }
-                request.post(errorOptions, (err) => {
-                    console.log('[!] SENT ERROR')
-                    })
-                }else if(message == '2'){
-                const payOptions = {
-                    url: url,
-                    method: 'POST',
-                    json: true,
-                    body: {
-                        "recipient": {
-                            "id": senderID
-                        },
-                        "message": {
-                            "text": "Enter Your Phone Number"
-                        }
-                    }
-                }
-                request.post(payOptions, () => {
-                    console.log('[+] 1 Option Gone');
-                })
-            }
-            else if(message == '1'){
-                const otpOptions = {
-                    url: url,
-                    method: 'POST',
-                    json: true,
-                    body: {
-                        "recipient": {
-                            "id": senderID
-                        },
-                        "message": {
-                            "text": `Enter your phone number and otp as follows:\n{Phone Number}, {OTP}`
-                        }
-                    }
-                }
-                request.post(otpOptions, (err, res) => {
-                    if(!err){
-                        console.log('[+] USER CHOSE 2')
-                    }else{
-                        console.log(err);
-                    }
-                })
-            }
-            else if(message == '3'){
-                const locOptions = {
-                    url: url,
-                    method: 'POST',
-                    json: true,
-                    body: {
-                        "recipient": {
-                            "id": senderID
-                        },
-                        "message": {
-                            "text": `${text}`
-                        }
-                    }
-                }
-                request.post(locOptions, (err) => {
-                    console.log('[+] LOCATION SENT')
-                })
-            }
-            else if(message == '4'){
-                const menuOptions = {
-                    url: url,
-                    method: 'POST',
-                    json: true,
-                    body: {
-                        "recipient": {
-                            "id": senderID
-                        },
-                        "message": {
-                            "text": `Hi Sir\n1: Verify OTP\n2: Get Repayment Info\n3: Locations\n4: Menu`
-                        }
-                    }
-                }
-                request.post(menuOptions, (err) => {
-                    console.log('[+] MENU SENT')
-                })
-            }
-            else if(message.length > 16 && message.length < 21){
-                const otpNum = message.split(/[, ]+/)
-                // console.log(otpNum);
-                // console.log(otpNum[0])
-                // console.log(otpNum[1])
-                // const mess = Object.keys(otpNum)
-                const sendOtp = {
-                    url: 'http://13.52.218.164:8000/api/v1/clients/verify-otp',
-                    method: 'POST',
-                    json: true,
-                    body: {
-                        "phone": `${otpNum[0]}`,
-                        "otp": `${otpNum[1]}`,
-                        "psid": senderID,
-                        "name": clientName,
-                        "profile_pic": clientProUrl,
-                        "message_id": message_id
-                    }
-                }
-                console.log(`OTP SHIT: ${JSON.stringify(sendOtp)}`)
-                request.post(sendOtp, (err, res) => {
-                    // const otpResp = JSON.stringify(res)
-                    console.log(`res code ${res.statusCode}`)
-                    if(res.statusCode != 400){
-                        const rightOTP = {
-                            url: url,
-                            method: 'POST',
-                            json: true,
-                            body: {
-                                "recipient": {
-                                    "id": senderID
-                                },
-                                "message": {
-                                    "text": `OTP Verified`
-                                }
-                            }
-                        }
-                        request.post(rightOTP, (err) => {
-                            console.log('[+] OTP VERIFIED')
-                        })
-                    }else{
-                        const rightOTP = {
-                            url: url,
-                            method: 'POST',
-                            json: true,
-                            body: {
-                                "recipient": {
-                                    "id": senderID
-                                },
-                                "message": {
-                                    "text": `Wrong OTP`
-                                }
-                            }
-                        }
-                        request.post(rightOTP, (err) => {
-                            console.log('[!] Wrong OTP')
-                        })
-                    }
-                })
-            }
-            else{
-                const options = {
-                    url: url,
-                    method: 'POST',
-                    json: true,
-                    body: {
-                        "recipient": {
-                            "id": senderID
-                        },
-                        "message": {
-                            "text": `your debt: ${debt}\nyour expire date: ${expireDate}`,
-                        }
-                    }
-                }
-                request.post(options, () => {
-                    console.log('[+] DEBT GONE');
-                })
-            }
-        })
+    try {
+      const response = await axios.get(nameUrl);
+      console.log("Data > ", response.data);
+      clientName = response.data.name;
+      clientProUrl = response.data.profile_pic;
+      console.log(`CLIENTNAME > ${clientName}`);
+      console.log(`PROFILE LINK > ${clientProUrl}`);
+    } catch (err) {
+      console.log(err);
     }
-}
+
+    try {
+      const options = {
+        url: `https://graph.facebook.com/v15.0/${senderID}/conversations`,
+        qs: {
+          access_token:
+            "EAALFFhY8vPkBAHcPoC4Th2SXmHRMozuahQcHj6omdc5kkzkNME58sPHsS0VUFO6qCNVsZCdFIUkASMBSYmQLKMtIHteSXQUgkCPvjhZACmrSd7z8MiBleKUlHCgBZCOXmkP5hSCiZADGnwlBspJnJCxUF3WH1Qa9ZAbin7SAa6wCtmM1XZCf08elmy2jZCUefsYq3cWxZAZCbVfXZA4t2LJ9EO",
+        },
+      };
+
+      request(options, function (error, body) {
+        if (error) throw new Error(error);
+
+        const data = JSON.parse(body).data;
+        if (data.length > 0) {
+          const link = data[0].link;
+          console.log(link);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    var names = [];
+    var resLocations = [];
+    var workHours = [];
+    try {
+      const resp = await axios.post(
+        "http://13.52.218.164:8000/api/v1/terminals/getLocations"
+      );
+      const locations = resp.data.data;
+      for (const shits in locations) {
+        const lmao = JSON.parse(JSON.stringify(locations[shits]));
+        names.push(lmao.name);
+        resLocations.push(lmao.location);
+        workHours.push(lmao.working_hours);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    var text = "";
+    for (let i = 0; i < names.length; i++) {
+      text +=
+        "\n" +
+        names[i] +
+        ", Locations: " +
+        resLocations[i] +
+        ", Work Hours: " +
+        workHours[i] +
+        "\n";
+    }
+
+    const backurl = "http://13.52.218.164:8000/api/v1/loans/getByPhone";
+    const backOptions = {
+      url: backurl,
+      method: "POST",
+      json: {
+        phone: number,
+        psid: senderID,
+        name: clientName,
+        profile_pic: clientProUrl,
+        message_id: link,
+      },
+    };
+    request.post(backOptions, (err, body) => {
+      if (!err) {
+        console.log(JSON.stringify(body));
+        tmp = JSON.stringify(body);
+        const obj = JSON.parse(tmp);
+        try {
+          console.log("FSAFDA> ", obj.body.data.repayment.current_pay_amount);
+          debt = obj.body.data.repayment.current_pay_amount;
+          dbUserName = obj.body.data.repayment.customer_name;
+          expireDate = obj.body.data.repayment.current_pay_date;
+        } catch (err) {
+          debt = null;
+        }
+      } else {
+        console.log(err);
+      }
+
+      if (
+        !debt &&
+        message != "2" &&
+        message != "1" &&
+        message != "3" &&
+        message != "4" &&
+        message.length < 16
+      ) {
+        const errorOptions = {
+          url: url,
+          method: "POST",
+          json: true,
+          body: {
+            recipient: {
+              id: senderID,
+            },
+            message: {
+              text: `${message} Is Not Registered\nBut You Can Register, Our Loan Amounts: \n440PHP\n660PHP\n880PHP\n1100PHP\n1300PHP\n1700PHP`,
+            },
+          },
+        };
+        request.post(errorOptions, (err) => {
+          console.log("[!] SENT ERROR");
+        });
+      } else if (message == "2") {
+        const payOptions = {
+          url: url,
+          method: "POST",
+          json: true,
+          body: {
+            recipient: {
+              id: senderID,
+            },
+            message: {
+              text: "Enter Your Phone Number",
+            },
+          },
+        };
+        request.post(payOptions, () => {
+          console.log("[+] 1 Option Gone");
+        });
+      } else if (message == "1") {
+        const otpOptions = {
+          url: url,
+          method: "POST",
+          json: true,
+          body: {
+            recipient: {
+              id: senderID,
+            },
+            message: {
+              text: `Enter your phone number and otp as follows:\n{Phone Number}, {OTP}`,
+            },
+          },
+        };
+        request.post(otpOptions, (err, res) => {
+          if (!err) {
+            console.log("[+] USER CHOSE 2");
+          } else {
+            console.log(err);
+          }
+        });
+      } else if (message == "3") {
+        const locOptions = {
+          url: url,
+          method: "POST",
+          json: true,
+          body: {
+            recipient: {
+              id: senderID,
+            },
+            message: {
+              text: `${text}`,
+            },
+          },
+        };
+        request.post(locOptions, (err) => {
+          console.log("[+] LOCATION SENT");
+        });
+      } else if (message == "4") {
+        const menuOptions = {
+          url: url,
+          method: "POST",
+          json: true,
+          body: {
+            recipient: {
+              id: senderID,
+            },
+            message: {
+              text: `Hi Sir\n1: Verify OTP\n2: Get Repayment Info\n3: Locations\n4: Menu`,
+            },
+          },
+        };
+        request.post(menuOptions, (err) => {
+          console.log("[+] MENU SENT");
+        });
+      } else if (message.length > 16 && message.length < 21) {
+        const otpNum = message.split(/[, ]+/);
+        // console.log(otpNum);
+        // console.log(otpNum[0])
+        // console.log(otpNum[1])
+        // const mess = Object.keys(otpNum)
+        const sendOtp = {
+          url: "http://13.52.218.164:8000/api/v1/clients/verify-otp",
+          method: "POST",
+          json: true,
+          body: {
+            phone: `${otpNum[0]}`,
+            otp: `${otpNum[1]}`,
+            psid: senderID,
+            name: clientName,
+            profile_pic: clientProUrl,
+            message_id: link,
+          },
+        };
+        console.log(`OTP SHIT: ${JSON.stringify(sendOtp)}`);
+        request.post(sendOtp, (err, res) => {
+          // const otpResp = JSON.stringify(res)
+          console.log(`res code ${res.statusCode}`);
+          if (res.statusCode != 400) {
+            const rightOTP = {
+              url: url,
+              method: "POST",
+              json: true,
+              body: {
+                recipient: {
+                  id: senderID,
+                },
+                message: {
+                  text: `OTP Verified`,
+                },
+              },
+            };
+            request.post(rightOTP, (err) => {
+              console.log("[+] OTP VERIFIED");
+            });
+          } else {
+            const rightOTP = {
+              url: url,
+              method: "POST",
+              json: true,
+              body: {
+                recipient: {
+                  id: senderID,
+                },
+                message: {
+                  text: `Wrong OTP`,
+                },
+              },
+            };
+            request.post(rightOTP, (err) => {
+              console.log("[!] Wrong OTP");
+            });
+          }
+        });
+      } else {
+        const options = {
+          url: url,
+          method: "POST",
+          json: true,
+          body: {
+            recipient: {
+              id: senderID,
+            },
+            message: {
+              text: `your debt: ${debt}\nyour expire date: ${expireDate}`,
+            },
+          },
+        };
+        request.post(options, () => {
+          console.log("[+] DEBT GONE");
+        });
+      }
+    });
+  }
+};
