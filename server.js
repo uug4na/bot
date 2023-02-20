25b5033907771541b5be5865124cfe438e073e22
Uuganbayar <uug4na@gmail.com>
2023-02-19 19:12:14 -0800

    gets port from env

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-const express = require('express')
-// const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app.use(morgan('dev')); 
-app.use(bodyParser.urlencoded({ extended:false }));
-app.use(bodyParser.json()); 
+require("./routes/webhook_verify")(app);
 
-require('./routes/webhook_verify')(app);
-
-app.listen(9001, function() {
-  console.log('Application running on port: 9001');
+app.listen(process.env.PORT, function () {
+  console.log("Application running on port: ", process.env.PORT);
 });
