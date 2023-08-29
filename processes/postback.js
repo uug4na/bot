4c5c07904aedd8df7530f692f520d133b25a80f3
Uuganbayar <uug4na@gmail.com>
2023-08-29 01:34:08 -0700

    Update postback.js

--- processes/postback.js
+++ processes/postback.js
@@ -1,29 +1,57 @@
-const request = require('request');
+const request = require("request");
 
- module.exports = function processPostback(event) {
-     const senderID = event.sender.id;
-     const payload = event.postback.payload;
-     const url = 'https://graph.facebook.com/v15.0/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN
-     console.log("shitting")
-     console.log(payload)
-     console.log(senderID)
-     if (payload === 'GET_STARTED_PAYLOAD') {
-            const options = {
-                url: url,
-                method: 'POST',
-                json: true,
-                body: {
-                    "recipient": {
-                        "id": senderID
-                    },
-                    "message": {
-                        "text": `Hi Sir\n1: Verify OTP\n2: Get Repayment Info\n3: Locations\n4: Menu`
-                    }
-                }
-            }
-            request.post(options, (err) => {
-                console.log('[+] SENT')
-            })
-            console.log("OSIJFIDS")
-        };
-     }
+module.exports = function processPostback(event) {
+  const senderID = event.sender.id;
+  const payload = event.postback.payload;
+  const url =
+    "https://graph.facebook.com/v15.0/me/messages?access_token=" +
+    process.env.PAGE_ACCESS_TOKEN;
+  console.log(payload);
+  console.log(senderID);
+  if (payload === "GET_STARTED_PAYLOAD") {
+    const options = {
+      url: url,
+      method: "POST",
+      json: true,
+      body: {
+        recipient: {
+          id: senderID,
+        },
+        message: {
+          attachment: {
+            type: "template",
+            payload: {
+              template_type: "button",
+              text: "Hi Sir, how can I assist you?",
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Verify OTP",
+                  payload: "VERIFY_OTP_PAYLOAD", 
+                },
+                {
+                  type: "postback",
+                  title: "Get Repayment Info",
+                  payload: "REPAYMENT_INFO_PAYLOAD", 
+                },
+                {
+                  type: "postback",
+                  title: "Locations",
+                  payload: "LOCATIONS_PAYLOAD", 
+                },
+                {
+                  type: "postback",
+                  title: "Menu",
+                  payload: "MENU_PAYLOAD", 
+                },
+              ],
+            },
+          },
+        },
+      },
+    };
+    request.post(options, (err) => {
+      console.log("[+] SENT");
+    });
+  }
+};
